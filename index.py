c2d25a447bc75cefff2d37861081eec10f3ce714
inlanger <inlanger@gmail.com>
2020-04-09 18:07:09 +0300

    fix publisher

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import redis
 import bottle
 import json
 
-client = redis.Redis(host='redis', port=6379, db=0)
+client = redis.Redis(host='127.0.0.1', port=6379, db=0)
 
 @bottle.get('/')
 def healthcheck():
@@ -10,7 +10,7 @@ def healthcheck():
 
 @bottle.post('/')
 def index():
-    client.publish('transactions', json.loads(bottle.request.json))
+    client.publish('transactions', json.dumps(bottle.request.json))
     return
 
 if __name__ == "__main__":
